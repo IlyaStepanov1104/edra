d7d91bd3bfc07af3be94cc1c44fb9e44ce97930e
ilya-1309 <ilyahtml@gmail.com>
2025-10-25 16:57:02 +0300

    test

--- frontend/src/entity/bot/types.ts
+++ frontend/src/entity/bot/types.ts
@@ -1,29 +1,7 @@
 export interface IBot {
-    id: number;
-    order: number;
-    slug: string;
-    title: string;
-    percent?: number;
-    comment?: string;
-    disabled?: boolean;
-}
-
-export interface IMessage {
-    id: number;
-    botId: number;
-    text: string;
-    isUser: boolean;
-    timestamp: Date;
-}
-
-export interface IBotStatisticsItem {
-    title: string;
-    percent: number;
-    comment: string;
-}
-
-export interface IBotStatistics {
-    title: string;
-    percent: number;
-    list: IBotStatisticsItem[];
+  id: number;
+  order: number;
+  slug: string;
+  title: string;
+  disabled?: boolean;
 }
