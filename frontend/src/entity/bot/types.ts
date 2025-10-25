cf0190ec8ed4eb2bcf570c32e95a29c8d4c27b1b
ilya-1309 <ilyahtml@gmail.com>
2025-10-25 16:58:49 +0300

    test

--- frontend/src/entity/bot/types.ts
+++ frontend/src/entity/bot/types.ts
@@ -5,3 +5,12 @@ export interface IBot {
   title: string;
   disabled?: boolean;
 }
+
+export interface IBotStatisticsItem {
+  id: number;
+  name: string;
+  progress: number;
+  value: number;
+  maxValue: number;
+  unit?: string;
+}
