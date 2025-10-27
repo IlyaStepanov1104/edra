65142baf6002af4085358fad4fa473ff130f7f7b
ilya-1309 <ilyahtml@gmail.com>
2025-10-27 06:24:12 +0300

    add backend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,19 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   
   // Enable CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://127.0.0.1:3000',
-      'http://127.0.0.1:3001'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  });
+  app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
