7591bd753e4d496e119218fd6bd2224960367f24
IlyaStepanov1104 <ilyahtml@gmai.com>
2025-12-28 20:40:57 +0300

    delete window

--- frontend/src/features/Chat/Chat.tsx
+++ frontend/src/features/Chat/Chat.tsx
@@ -59,7 +59,7 @@ export const Chat: FC = ({}) => {
     const chatContainerRef = useRef<HTMLDivElement>(null);
     const [qrModalOpen, setQrModalOpen] = useState(false);
     const [qrHash, setQrHash] = useState<string | null>(null);
-    const QRLink = `${window?.origin}/photo-upload/${qrHash}`;
+    const QRLink = `/photo-upload/${qrHash}`;
     const [qrResult, setQrResult] = useState<LatexResult | null>(null);
 
     useEffect(() => {
