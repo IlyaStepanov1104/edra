d10a8fbc9f1e135365e061ad5fe2de88d02edf7b
IlyaStepanov1104 <ilyahtml@gmai.com>
2025-12-28 20:46:38 +0300

    delete console.log

--- frontend/src/shared/lib/auth.ts
+++ frontend/src/shared/lib/auth.ts
@@ -2,11 +2,9 @@ import {v4 as uuidv4} from 'uuid';
 
 export const getUserToken = (): string | null => {
     let token = localStorage.getItem('token');
-    console.log("%c 1 --> Line: 5||auth.ts\n token: ","color:#f0f;", token);
 
     if (!token) {
         token = uuidv4();
-        console.log("%c 2 --> Line: 9||auth.ts\n NEW token: ","color:#0f0;", token);
         localStorage.setItem('token', token);
     }
 
