059d4fd492457a15c8310988980a5dbc9ff557c3
IlyaStepanov1104 <ilyahtml@gmai.com>
2025-12-28 03:04:56 +0300

    edit backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Enable CORS
-  app.enableCors({
-    origin: ['http://localhost:3001', 'https://edra-test.duckdns.org'], // разрешить оба домена для dev/prod
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization', // обязательно добавить Content-Type
-    credentials: true,
-  });
+    // Enable CORS
+    app.enableCors({
+        origin: ['http://localhost:3001', 'https://edra-test.duckdns.org'], // разрешить оба домена для dev/prod
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type,Authorization', // обязательно добавить Content-Type
+        credentials: true,
+    });
+    const port = process.env.PORT || 3000;
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
